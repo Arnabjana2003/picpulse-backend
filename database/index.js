637d5cb16a72ddb32192d910fd86882d0e60ca8d
Arnab Jana <arnabjana0099@gmail.com>
2024-03-18 11:51:47 +0530

    modify

--- database/index.js
+++ database/index.js
@@ -3,7 +3,7 @@ import config from "../config.js"
 
 const dbConnect = async ()=>{
     try {
-        await mongoose.connect(config.databaseUrl)
+        await mongoose.connect(`${config.databaseUrl}/picpulse`)
         console.log("Database connected to ",config.databaseUrl)
     } catch (error) {
         console.log("DATABASE CONNECTION ERROR::",error)
