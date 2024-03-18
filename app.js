a872c18e0c7082bec871b6a9375751e442563325
Arnab Jana <arnabjana0099@gmail.com>
2024-03-18 09:13:09 +0530

    cors setting uodated

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ import express from "express"
 import cors from "cors"
 
 const app = express()
+const allowedOrigins = ['http://localhost:5173', 'https://picpulse.vercel.app'];
+app.use(cors({
+    origin: function(origin, callback) {
+      // Check if the request origin is included in the allowed origins
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    }
+  }));
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
-// app.use(cors({origin:"http://localhost:5173"}))
-app.use(cors())
 
 app.get("/",(req,res)=>{
     res.send("Server running")
